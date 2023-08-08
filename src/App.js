29a6e9079326f5b3269b922299c8a43fecd904b0
WenerRodrigo <wener19.wr@gmail.com>
2023-08-08 20:03:57 -0300

    criando componentes tipografia e campo de texto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { CampoTexto } from "./components/CampoTexto/CampoTexto";
 import { Card } from "./components/Card/Card";
 import { ProvedorTema } from "./components/ProvedorTema/ProvedorTema";
 import { Styles } from "./components/StylesGlobal/Styles";
+import { Tipografia } from "./components/Tipografia/Tipografia";
 
 function App() {
   return (
     <ProvedorTema>
       <Styles />
       <Card>
-        <h1>Freelando</h1>
+        <Tipografia variante="h1" componente="h1">Freelando</Tipografia>
+        <Tipografia variante="body" componente="body">
+          Crie seu perfil gratuitamente para começar a trabalhar com os melhores freelancers.
+          Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho.
+        </Tipografia>
+        <CampoTexto titulo="Nome Completo"/>
       </Card>
     </ProvedorTema>
   );
