8d0071c221b12f5768a3b2d59c46f9d8e7cf3510
WenerRodrigo <wener19.wr@gmail.com>
2023-08-09 20:14:40 -0300

    Projeto concluido , criado rodapé , add icones , add cabeçalho e estilos globais

---
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+
+export const Cabecalho = styled.header`
+    background-color: ${props => props.theme.cores.primarias.a};
+    padding: ${props => props.theme.espacamentos.m};
+`
